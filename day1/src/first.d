36cf4aff9bd53872f768763840476cf6ab603e1e
Paul Backus <snarwin@gmail.com>
2019-12-01 21:03:46 -0500

    Refactor

--- day1/src/first.d
+++ day1/src/first.d
@@ -1,28 +1,25 @@
-module first;
-
 import std.range.primitives;
 import std.algorithm;
 
-int requiredFuel(int mass)
+int fuel(int mass)
 {
-	assert(mass >= 6);
-	return (mass / 3) - 2;
+	return max(0, (mass / 3) - 2);
 }
 
 unittest {
-	assert(requiredFuel(12) == 2);
-	assert(requiredFuel(14) == 2);
-	assert(requiredFuel(1969) == 654);
-	assert(requiredFuel(100756) == 33583);
+	assert(fuel(12) == 2);
+	assert(fuel(14) == 2);
+	assert(fuel(1969) == 654);
+	assert(fuel(100756) == 33583);
 }
 
-int totalFuel(R)(R masses)
+int fuelTotal(R)(R masses)
 	if (isInputRange!R)
 {
-	return masses.map!requiredFuel.sum;
+	return masses.map!fuel.sum;
 }
 
 unittest {
 	int[] exampleMasses = [12, 14, 1969, 100756];
-	assert(totalFuel(exampleMasses) == 2 + 2 + 654 + 33583);
+	assert(fuelTotal(exampleMasses) == 2 + 2 + 654 + 33583);
 }
--- day1/src/main.d
+++ day1/src/main.d
@@ -1,18 +1,17 @@
-module main;
-
 import first;
 
-import std.stdio;
-import std.conv;
 import std.algorithm;
+import std.conv;
+import std.getopt;
+import std.stdio;
 
 auto byInt(File input)
 {
 	return input.byLine.map!(to!int);
 }
 
-void main()
+void main(string[] args)
 {
 	auto masses = stdin.byInt;
-	writeln(masses.totalFuel);
+	masses.fuelTotal.writeln;
 }
