73b0cf12b4069a776fab883cb772befa4d32e8b4
Paul Backus <snarwin@gmail.com>
2019-12-02 00:55:54 -0500

    Update old module names in unit tests

--- day1/src/common.d
+++ day1/src/common.d
@@ -8,15 +8,15 @@ int fuelTotal(alias fuel, R)(R masses)
 }
 
 unittest {
-	static import first;
+	static import one;
 
 	int[] exampleMasses = [12, 14, 1969, 100756];
-	assert(fuelTotal!(first.fuel)(exampleMasses) == 2 + 2 + 654 + 33583);
+	assert(fuelTotal!(one.fuel)(exampleMasses) == 2 + 2 + 654 + 33583);
 }
 
 unittest {
-	static import second;
+	static import two;
 
 	int[] exampleMasses = [14, 1969, 100756];
-	assert(fuelTotal!(second.fuel)(exampleMasses) == 2 + 966 + 50346);
+	assert(fuelTotal!(two.fuel)(exampleMasses) == 2 + 966 + 50346);
 }
